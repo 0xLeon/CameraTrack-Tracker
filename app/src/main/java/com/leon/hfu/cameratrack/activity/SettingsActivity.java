ddc98122a81503d5894e586228ec8893607e871a
Stefan <Stefan_Hahn1@gmx.net>
2017-12-13 12:31:57 +0100

    Handle configuration changes in settings activity

--- app/src/main/java/com/leon/hfu/cameratrack/activity/SettingsActivity.java
+++ app/src/main/java/com/leon/hfu/cameratrack/activity/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.leon.hfu.cameratrack.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,10 @@ public class SettingsActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 
 		this.getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
 	}
 }
